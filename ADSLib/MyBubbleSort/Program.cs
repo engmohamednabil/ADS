23017b8124872be1fde13a904a4c696a1eaededc
Mnabil <mohnab2006@gmail.com>
2017-01-20 03:53:09 +0200

    combine all projects under the solution folder

---
+++ ADSLib/MyBubbleSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyBubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k,temp;
+            int[] myArray = { 8 , 1 , 10 , 7 , 9 , 5 };
+            for (j = myArray.Length-1; j > 1; j--)
+            {
+                for (i = 0; i < j; i++)
+                {
+                    k = i;
+                    if (myArray[k++] > myArray[k])
+                    {
+                        temp = myArray[k];
+                        myArray[k] = myArray[k-1];
+                        myArray[k-1] = temp;
+                    }
+                }
+                display(myArray);
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+        private static void display(int[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+                Console.Write(myArray[i].ToString() + " ");
+        }
+    }
+}
