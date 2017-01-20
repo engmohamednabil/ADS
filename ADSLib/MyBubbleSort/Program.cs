de49168cbdc744d98905d8bc24e5413529cd2d10
Mnabil <mohnab2006@gmail.com>
2017-01-20 04:48:44 +0200

    .

--- ADSLib/MyBubbleSort/Program.cs
+++ ADSLib/MyBubbleSort/Program.cs
@@ -6,8 +6,8 @@ namespace MyBubbleSort
     {
         static void Main(string[] args)
         {
-            int i, j, k,temp;
-            int[] myArray = { 8 , 1 , 10 , 7 , 9 , 5 };
+            int i, j, k, temp;
+            int[] myArray = { 8 , 1 , 10 , 7 , 9 , 5 , 2};
             for (j = myArray.Length-1; j > 1; j--)
             {
                 for (i = 0; i < j; i++)
@@ -22,7 +22,6 @@ namespace MyBubbleSort
                 }
                 display(myArray);
                 Console.WriteLine();
-
             }
             Console.ReadLine();
         }
